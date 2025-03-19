7dd2770fefaa203eb3c4240873f2cb1b98f371f2
[maykelsantoz] <maykelsantoz@gmail.com>
2025-03-18 22:50:36 -0300

    feat: add initial cart item component (without functionality)

---
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -0,0 +1,55 @@
+import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+import { CartProduct } from "../contexts/cart";
+
+interface CartItemProps {
+    product: CartProduct;
+}
+const CartProductItem = ({product}: CartItemProps) => {
+    return ( 
+        <div className="flex justify-between items-center">
+            <div className="flex items-center gap-3">
+            <div className="relative h-20 w-20 bg-gray-100 rounded-xl ">
+                <Image src={product.imageUrl} alt={product.name} fill />
+            </div>
+            <div className="space-y-1">
+                <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
+                <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
+                <div className="flex items-center gap-1 text-center">
+                    <Button
+                        size="icon"
+                        variant="outline"
+                        className="h-7 w-7 rounded-lg"
+        
+                    >
+                    <ChevronLeftIcon />
+                    </Button>
+                        <p className="w-5 text-xs">{product.quantity}</p>
+                    <Button
+                        size="icon"
+                        variant="destructive"
+                        className="h-7 w-7 rounded-lg"
+                    >
+                        <ChevronRightIcon />
+                    </Button>
+                </div>
+            </div>
+            </div>
+            <Button
+                size="icon"
+                variant="outline"
+                className="h-7 w-7 rounded-lg" 
+
+            >
+                <TrashIcon />
+            </Button>
+        </div>
+     );
+}
+
+ 
+export default CartProductItem;
